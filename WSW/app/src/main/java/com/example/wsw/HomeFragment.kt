b8aa493d5be37dddbc967524b1d8dc6c94f15791
이지원 <ijiwon@ijiwon-ui-MacBookPro.local>
2021-08-11 15:00:54 +0900

    bottom nav 추가 & main->home fragment 수정

---
+++ WSW/app/src/main/java/com/example/wsw/HomeFragment.kt
@@ -0,0 +1,34 @@
+package com.example.wsw
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HomeFragment :Fragment(){
+
+    companion object{
+        const val TAG: String = "HomeFragment"
+
+        fun newInstance(): HomeFragment{
+            return HomeFragment()
+        }
+    }
+    //메모리에 올라갔을 때
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    // 프레그먼트를 안고 있는 액티비티에 붙었을 때
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        return view
+    }
+}
--- WSW/app/src/main/java/com/example/wsw/MainActivity.kt
+++ WSW/app/src/main/java/com/example/wsw/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.wsw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+
+    private lateinit var homeFragment: HomeFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        homeFragment = HomeFragment.newInstance()
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_frame, homeFragment).commit()
+        bottom_nav.setOnNavigationItemSelectedListener(onBottomNavItemSelectedListener)
     }
+    //바텀 네비게이션 아이템 클릭 리스너 설정
+    private val onBottomNavItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        when(it.itemId){
+            R.id.menu_home ->{
+                homeFragment = HomeFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_frame, homeFragment).commit()
+            }
+        }
+
+        true
+    }
+
 }
