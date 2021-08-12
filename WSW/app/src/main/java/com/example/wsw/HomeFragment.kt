2bb64e2ff6f85e775760d4a6fe2406a473c57d1d
hinhyu <hinhyu@naver.com>
2021-08-12 20:34:26 +0900

    Random Message

--- WSW/app/src/main/java/com/example/wsw/HomeFragment.kt
+++ WSW/app/src/main/java/com/example/wsw/HomeFragment.kt
@@ -1,11 +1,23 @@
 package com.example.wsw
 
 import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.fragment_home.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
 class HomeFragment :Fragment(){
 
@@ -29,7 +41,33 @@ class HomeFragment :Fragment(){
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
+        Log.e("fragment","create view")
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.e("fragment","view created")
+
+        init()
+    }
+
+
+    fun init() {
+
+        val settings: SharedPreferences = activity!!.getSharedPreferences("gitcat", AppCompatActivity.MODE_PRIVATE)
+
+        //Notification
+
+        //img_notification.setOnClickListener { view ->
+        //    val intent = Intent(activity,NotifyActivity::class.java)
+        //    startActivity(intent)
+        //}
+
+        img_account.setOnClickListener { view ->
+            val intent = Intent(activity, MypageFragment::class.java)
+            startActivity(intent)
+        }
     }
 }
