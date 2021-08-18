e2bd1d6f8f6d2eed363345b2c4da5d58120d7685
이지원 <ijiwon@ijiwon-ui-MacBookPro.local>
2021-08-18 18:59:36 +0900

    retrofit api 설정, sign up

--- WSW/app/src/main/java/com/example/wsw/api/ApiService.kt
+++ WSW/app/src/main/java/com/example/wsw/api/ApiService.kt
@@ -1,4 +1,17 @@
 package com.example.wsw.api
 
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface ApiService {
+
+    @FormUrlEncoded
+    @POST("user/signup")
+    fun requestRegister(@Field("user_name") user_name:String,
+                        @Field("user_email")user_email:String,
+                        @Field("user_pw") user_pw:String,
+                        @Field("user_check") user_check:Int): Call<Boolean>
+
 }
