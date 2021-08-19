8a5c6e484c6496317a37a0a2645b4b575a36ccbe
Tae Young <k55416325@gamil.com>
2021-08-20 07:10:31 +0900

    태영 완료

--- WSW/app/src/main/java/com/example/wsw/ui/reviewboard/ReviewBoardAdapter.kt
+++ WSW/app/src/main/java/com/example/wsw/ui/reviewboard/ReviewBoardAdapter.kt
@@ -1,16 +1,38 @@
 package com.example.wsw.ui.reviewboard
 
+import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wsw.R
+import com.example.wsw.data.reviewListGetData
 import com.example.wsw.ui.questionboard.QuestionBoardPostActivity
+import org.w3c.dom.Text
 
-class ReviewBoardAdapter() : RecyclerView.Adapter<ReviewBoardAdapter.ReviewBoardViewHolder>(){
+class ReviewBoardAdapter(val context:Context, val review_list:ArrayList<reviewListGetData>) : RecyclerView.Adapter<ReviewBoardAdapter.ReviewBoardViewHolder>(){
+
+    fun imakeformat(date:String):String{
+        //date를 받아온다
+        var date = date
+
+        //요일
+        var MM = date.substring(5,7)
+        var DD = date.substring(8,10)
+
+        //시간
+        var h = date.substring(11,13)
+        var m = date.substring(14,16)
+
+        //완성
+        var complete = MM+"/"+DD+" "+h+":"+m
+
+        return complete
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewBoardViewHolder {
         val inflatedView = LayoutInflater.from(parent.context).inflate(R.layout.listitem_review_board, parent, false)
@@ -18,15 +40,38 @@ class ReviewBoardAdapter() : RecyclerView.Adapter<ReviewBoardAdapter.ReviewBoard
     }
 
     override fun onBindViewHolder(holder: ReviewBoardAdapter.ReviewBoardViewHolder, position: Int) {
+        holder.bind(review_list[position], context)
+
         holder.itemView.setOnClickListener{
             val intent = Intent(holder.itemView.context, ReviewBoardPostActivity::class.java)
             ContextCompat.startActivity(holder.itemView.context,intent,null)
         }
     }
 
-    override fun getItemCount() = 10
+    override fun getItemCount(): Int{
+        return review_list.size
+    }
 
-    class ReviewBoardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ReviewBoardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val review_company = itemView.findViewById<TextView>(R.id.tv_company) //회사
+        val review_content = itemView.findViewById<TextView>(R.id.tv_contentPreview)
+        val review_like = itemView.findViewById<TextView>(R.id.txt_review_like)
+        val review_comm = itemView.findViewById<TextView>(R.id.txt_review_comm)
+        val review_date = itemView.findViewById<TextView>(R.id.tv_time)
+
+        fun bind(item: reviewListGetData, context: Context){
+            var rev_content = item.review_content
+
+            if (rev_content.length > 30){
+                review_content.text = rev_content.substring(rev_content.indexOf(":")+1,30).plus("...")
+            }else{
+                review_content.text = rev_content.substring(rev_content.indexOf(":")+1,rev_content.length)
+            }
+            review_company.text = rev_content.substring(0,rev_content.indexOf(":"))
+            review_like.text = item.review_like.toString()
+            review_comm.text = item.reviewe_CMcount.toString()
+            review_date.text = imakeformat(item.review_date)
+        }
 
     }
 
--- WSW/app/src/main/java/com/example/wsw/ui/reviewboard/ReviewBoardFragment.kt
+++ WSW/app/src/main/java/com/example/wsw/ui/reviewboard/ReviewBoardFragment.kt
@@ -2,24 +2,55 @@ package com.example.wsw.ui.reviewboard
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wsw.R
+import com.example.wsw.api.ApiServicempl
+import com.example.wsw.data.SecretListGetData
+import com.example.wsw.data.reviewListGetData
+import com.example.wsw.ui.secretboard.SecretBoardAdapter
 import com.example.wsw.ui.secretboard.post.SecretBoardWriteActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import retrofit2.Call
+import retrofit2.Response
 
 class ReviewBoardFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_review_board, container, false)
         clickFab(view)
 
+        ApiServicempl.api.reviewList().enqueue(object: retrofit2.Callback<ArrayList<reviewListGetData>> {
+            override fun onResponse(
+                call: Call<ArrayList<reviewListGetData>>,
+                response: Response<ArrayList<reviewListGetData>>
+            ) {
+//                var first = response.body()!! //이 문법은 하나의 객체를 가져옵니다..
+//                Log.d("성공", first.toString())
+
+                val adapter = ReviewBoardAdapter(view.context, response.body()!!)
+                val rcv_reviewBoard = view!!.findViewById<RecyclerView>(R.id.rcv_reviewBoard)
+                rcv_reviewBoard.adapter = adapter
+
+                val lm = LinearLayoutManager(view.context)
+                rcv_reviewBoard.layoutManager = lm
+                rcv_reviewBoard.setHasFixedSize(true)
+            }
+
+            override fun onFailure(call: Call<ArrayList<reviewListGetData>>, t: Throwable) {
+                t.printStackTrace()
+                Log.d("실패",t.message.toString())
+            }
+        })
+
         //Adapter연결 : 태영
-        val adapter = ReviewBoardAdapter()
-        val rcv_reviewBoard = view!!.findViewById<RecyclerView>(R.id.rcv_reviewBoard)
-        rcv_reviewBoard.adapter = adapter
+//        val adapter = ReviewBoardAdapter()
+//        val rcv_reviewBoard = view!!.findViewById<RecyclerView>(R.id.rcv_reviewBoard)
+//        rcv_reviewBoard.adapter = adapter
 
         return view
     }
